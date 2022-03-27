eaa3c9c95b57d7ba4484e847061c0e5112e49472
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-27 00:25:27 -0400

    Delete unused files

--- src/enums/VariantInventoryPolicy.ts
+++
@@ -1,8 +0,0 @@
-export class VariantInventoryPolicy {
-    Continue: string; // a product cannot be purchased after its inventory level reaches zero
-    Deny: string; // a product can be purchased after its inventory level reaches zero: negative inventory levels are enabled
-    constructor() {
-        this.Continue = 'continue';
-        this.Deny = 'deny';
-    }
-}
